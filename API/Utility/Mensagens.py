95f82f7427a918706c1b3c6b3078800fe3bd7677
DougDev <douglas.ofc.2004@gmail.com>
2025-03-29 07:00:12 +0000

    Adiciona algumas mensagens que vão ser utilizadas no fluxo de diálogo.

---
+++ API/Utility/Mensagens.py
@@ -0,0 +1,37 @@
+Menu_inicial = """
+ Olá! Eu sou o ZecoLógico, seu assistente
+ virtual de sustentabilidade e meio ambiente
+ do Governo de Goiânia. Que bom ter você por aqui!
+ Estou aqui para te ajudar a descobrir dicas práticas,
+ promover hábitos sustentáveis e conectar você aos 
+ serviços públicos que fazem a diferença no nosso dia a dia.
+ Vamos juntos deixar Goiás mais verde? Em que posso ajudar hoje?
+
+1 - Ver o turno da coleta de lixo no seu bairro.
+2 - Ver o turno da coleta seletiva no seu bairro. 
+3 - Descarte de resíduos.
+4 - Fazer uma Denúncia."""
+
+Pedir_cep_coleta_organica = """
+Me informa o seu CEP, pra eu poder verificar
+ os turnos da coleta na sua região.  
+"""
+Pedir_cep_coleta_seletiva = """
+Me informa o seu CEP, pra eu poder verificar
+ os turnos da coleta seletiva na sua região.  
+"""
+Menu_descarte_de_residuos = """
+Que bom que você quer discartar seus resíduos
+de forma ecologica e sustentável, escolha uma das
+opções de descarte a baixo. 
+1 - Descartar em um ecoponto.
+2 - Instituições parceiras.
+3 - Progamas da Prefeitura. """
+
+Menu_denuncia = """
+Em qual dos tópicos se equadra a sua denúncia: 
+
+1 - Descarte irregular de lixo. 
+
+
+"""
